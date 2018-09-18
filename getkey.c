e131fee3b1a290ae500c530fa1bed696b1d61e7c
jjmccormick <jjmccormick@crimson.ua.edu>
2018-09-18 12:23:30 -0500

    Program Working

--- getkey.c
+++ getkey.c
@@ -5,8 +5,9 @@
 #include <string.h>
 #include <stdlib.h>
 
-int encrypt(unsigned char* cipher, unsigned char * plaintext, unsigned char* key, unsigned char * iv, int plainLength);
+int encrypt(unsigned char *plaintext, int plainlen, unsigned char *key,unsigned char *iv, unsigned char *ciphertext);
 void padWord(char *word);
+void handleErrors(void);
 
 int main (int argc, char** argv)
 {
@@ -15,7 +16,7 @@ int main (int argc, char** argv)
         unsigned char *plain = "This is a top secret."; //Initialize our plaintext
         //unsigned char cipher[] = {0x8d, 0x20, 0xe5, 0x05, 0x6a, 0x8d, 0x24, 0xd0, 0x46, 0x2c, 0xe7, 0x4e, 0x49, 0x04, 0xc1, 0xb5, 0x13, 0xe1, 0x0d, 0x1d, 0xf4, 0xa2, 0xef, 0x2a, 0xd4, 0x54, 0x0f, 0xae, 0x1c, 0xa0, 0xaa, 0xf9}; // Initialize our cipher
         unsigned char *cipher =  "8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9";
-        unsigned char encryptBuf[128]; //Initialize a place to store ciphertext output
+        unsigned char encryptBuf[100]; //Initialize a place to store ciphertext output
         if(argc < 2)
         {
             fprintf(stderr, "No word list argument provided");
@@ -45,9 +46,9 @@ int main (int argc, char** argv)
             else 
                 padWord(word);
             
-            cipherLength = encrypt(plain,encryptBuf, word, iv, strlen(plain));
+            cipherLength = encrypt(plain,strlen(plain), word, iv, encryptBuf);
             
-            unsigned char crypto[128];
+            unsigned char crypto[50];
             for(int i = 0 ; i < cipherLength; i++)
             {
                 sprintf(crypto+i*2, "%02x", encryptBuf[i]);
@@ -57,7 +58,7 @@ int main (int argc, char** argv)
             //printf("\n Word: %s\nOutput: %s \n Cipher: %s\n",word, crypto, cipher);
             if(0 == strcmp(crypto, cipher))
             {
-                printf("Key found! The key is: %s", word);
+                printf("Key found! The key is: %s\n", word);
                 EVP_cleanup();
                 ERR_free_strings();
                 return 0;
@@ -69,37 +70,36 @@ int main (int argc, char** argv)
 }
 
 
-int encrypt(unsigned char* plain, unsigned char * cipher, unsigned char* key, unsigned char * iv, int plainLength)
+void handleErrors(void)
 {
-    int length;
-    int cipherLength;
-    
-    EVP_CIPHER_CTX * ctx;
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-    {
-        ERR_print_errors_fp(stderr);
-        abort();
-    }
-    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(),NULL, key, iv)) //Initialize with our cipher type, key, and iv
-    {
-        ERR_print_errors_fp(stderr);
-        abort();
-    }
-    if(1 != EVP_EncryptUpdate(ctx, cipher, &length, plain , plainLength)) //Call Decrypt Update with our cipher
-    {
-        ERR_print_errors_fp(stderr);
-        abort();
-    }
-    cipherLength = length;
-    if(1 != EVP_DecryptFinal_ex(ctx, cipher + length, &length))//Finish decrypting
-    {
-        ERR_print_errors_fp(stderr);
-        abort();
-    }
-    cipherLength += length;
-    EVP_CIPHER_CTX_free(ctx);
-    return cipherLength;
-    
+  ERR_print_errors_fp(stderr);
+  abort();
+}
+
+int encrypt(unsigned char *plaintext, int plainlen, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+{
+  EVP_CIPHER_CTX *ctx;
+
+  int len;
+
+  int cipherlen;
+
+  if(!(ctx = EVP_CIPHER_CTX_new()))
+      handleErrors();
+
+  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv))
+    handleErrors();
+
+  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plainlen))
+    handleErrors();
+  cipherlen = len;
+
+  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+  cipherlen += len;
+
+  EVP_CIPHER_CTX_free(ctx);
+
+  return cipherlen;
 }
 
  
